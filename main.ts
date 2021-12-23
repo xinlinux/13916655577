ae1af3846d107d23be64224bf08edb45c6fa8543
xinlinux <xinli13621862166@hotmail.com>
2021-12-23 17:41:48 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    xuehao += 5
+})
+input.onButtonPressed(Button.AB, function () {
+    xuehao += 10
+})
+input.onButtonPressed(Button.B, function () {
+    xuehao += 1
+})
+let xuehao = 0
